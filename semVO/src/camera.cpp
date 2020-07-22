39f53c2d4a90ee779a02b07b5030b21cc97f5616
jixingwu <1341582239@qq.com>
2020-07-22 16:47:36 +0800

    以yaml文件传入参数

--- semVO/src/camera.cpp
+++ semVO/src/camera.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "camera.h"
+#include "config.h"
+
+Camera::Camera()
+{
+    fx_ = Config::get<float>("camera.fx");
+    fy_ = Config::get<float>("camera.fy");
+    cx_ = Config::get<float>("camera.cx");
+    cy_ = Config::get<float>("camera.cy");
+
+}
