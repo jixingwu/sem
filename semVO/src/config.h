2f4f43ef8cf9f7cdb98fea3c54e138e928181d37
jixingwu <1341582239@qq.com>
2020-07-22 09:40:29 +0800

    finished config class to input necessary params

--- semVO/src/config.h
+++ semVO/src/config.h
@@ -4,8 +4,29 @@
 
 #ifndef SRC_CONFIG_H
 #define SRC_CONFIG_H
-// map cube
-class Frame;
 
+#include "camera.h"
+
+class Config
+{
+private:
+    static shared_ptr<Config> config_;
+    cv::FileStorage file_;
+
+    Config() {}
+
+public:
+    ~Config();
+
+    // set a new config file
+    static void setParameterFile(const std::string& filename);
+
+    // access the parameter values
+    template< typename T>
+    static T get(const std::string& key)
+    {
+        return T(Config::config_->file_[key]);
+    }
+};
 
 #endif //SRC_CONFIG_H
